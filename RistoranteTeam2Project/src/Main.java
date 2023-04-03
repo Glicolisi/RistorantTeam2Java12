a5ab7e022e585a1c512678f895e4b84538111a8e
AndreaFusari <andrea.fusari@hotmail.com>
2023-04-03 21:19:51 +0200

    creata classe SecondiPiatti e aggiunti 5 secondi piatti nella classe  Main

--- RistoranteTeam2Project/src/Main.java
+++ RistoranteTeam2Project/src/Main.java
@@ -1,9 +1,18 @@
 
+import Models.SecondiPiatti;
 import Models.primiPiatti;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        SecondiPiatti secondo1 = new SecondiPiatti("Carpaccio di filetto con rucola e scaglie di parmigiano",12.00);
+        SecondiPiatti secondo2= new SecondiPiatti("Grigliata di scampi e gamberi",16.0);
+        SecondiPiatti secondo3= new SecondiPiatti("Impepata di cozze",12.0);
+        SecondiPiatti secondo4= new SecondiPiatti("Filetto di manzo alla tartara",13.0);
+        SecondiPiatti secondo5= new SecondiPiatti("Nodino di vitello alla griglia",13.0);
+
+        System.out.println(SecondiPiatti.secondiPiatti);
 
 
 
---
+++ RistoranteTeam2Project/src/Models/SecondiPiatti.java
@@ -0,0 +1,34 @@
+package Models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SecondiPiatti {
+    private String nomeSecondo;
+    private double prezzoSecondo;
+   public static Map<String,Double> secondiPiatti = new HashMap<>();
+
+    public SecondiPiatti(String nomeSecondo, double prezzoSecondo) {
+        this.nomeSecondo = nomeSecondo;
+        this.prezzoSecondo = prezzoSecondo;
+        secondiPiatti.put(nomeSecondo,prezzoSecondo);
+    }
+
+    public String getNomeSecondo() {
+        return nomeSecondo;
+    }
+
+    public void setNomeSecondo(String nomeSecondo) {
+        this.nomeSecondo = nomeSecondo;
+    }
+
+    public double getPrezzoSecondo() {
+        return prezzoSecondo;
+    }
+
+    public void setPrezzoSecondo(double prezzoSecondo) {
+        this.prezzoSecondo = prezzoSecondo;
+    }
+
+
+    }
