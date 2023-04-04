8bbb21b9ba721ac953be57c335fed31b8de56455
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-04-04 18:46:36 +0200

    Uniformata la classe PrimiPiatti

--- RistoranteTeam2Project/src/Models/PrimiPiatti.java
+++ RistoranteTeam2Project/src/Models/PrimiPiatti.java
@@ -1,17 +1,48 @@
+//package RistoranteTeam2Project.src.Models;
 package Models;
-
 import java.util.*;
 
 public class PrimiPiatti {
 
-private Map<String,Double> Primi= new HashMap<>() ;
 
-private String nomePrimi;
-private Double prezzoPrimi;
+    private String nomePrimo;
+    private Double prezzoPrimo;
+    private static Map<String,Double> primiPiatti = new HashMap<>();
 
+    public PrimiPiatti(String nomePrimo, Double prezzoPrimo) {
+        this.nomePrimo = nomePrimo;
+        this.prezzoPrimo = prezzoPrimo;
+        primiPiatti.put(nomePrimo,prezzoPrimo);
+    }
 
+    public PrimiPiatti() {
+    }
 
-    public void print_Menu(){
+    public String getNomePrimo() {
+        return nomePrimo;
+    }
+
+    public void setNomePrimo(String nomePrimo) {
+        this.nomePrimo = nomePrimo;
+    }
+
+    public Double getPrezzoPrimo() {
+        return prezzoPrimo;
+    }
+
+    public void setPrezzoPrimo(Double prezzoPrimo) {
+        this.prezzoPrimo = prezzoPrimo;
+    }
+
+    public static Map<String, Double> getPrimiPiatti() {
+        return primiPiatti;
+    }
+
+    public void setPrimiPiatti(Map<String, Double> primiPiatti) {
+        this.primiPiatti = primiPiatti;
+    }
+
+    public static void print_Menu(){
 
         System.out.println("I Primi Piatti sono:");
 
