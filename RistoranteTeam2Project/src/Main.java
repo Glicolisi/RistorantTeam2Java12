b74a4cd5fe989ace8b97bf8512b25df360bff0b2
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-04-06 18:35:54 +0200

    Semplificazione Main

--- RistoranteTeam2Project/src/Main.java
+++ RistoranteTeam2Project/src/Main.java
@@ -1,9 +1,5 @@
 
-import Models.Antipasti;
-import Models.Bevande;
-import Models.Dolci;
-import Models.PrimiPiatti;
-import Models.SecondiPiatti;
+import Models.*;
 import org.w3c.dom.ls.LSOutput;
 
 
@@ -14,52 +10,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Antipasti antipasto1 = new Antipasti("Caprese", 5.00);
-        Antipasti antipasto2 = new Antipasti("Tagliere di salumi e formaggi", 10.00);
-        Antipasti antipasto3 = new Antipasti("Frittura di verdure miste", 6.00);
-        Antipasti antipasto4 = new Antipasti("Anelli di cipolla", 5.00);
-        Antipasti antipasto5 = new Antipasti("Antipasto della casa", 7.00);
-        Antipasti.printMenuAntipasti();
-
-        System.out.println("\n");
-
-        PrimiPiatti primo1 = new PrimiPiatti("Lasagne alla Bolognese", 12.0);
-        PrimiPiatti primo2 = new PrimiPiatti("Bucatini alla amatriciana", 11.0);
-        PrimiPiatti primo3 = new PrimiPiatti("Tortellini con panna", 14.0);
-        PrimiPiatti primo4 = new PrimiPiatti("Pennette alla vodka", 10.0);
-        PrimiPiatti primo5 = new PrimiPiatti("Trofie al pesto genovese", 10.0);
-        PrimiPiatti.print_Menu();
-
-        System.out.println("\n");
-
-        
-
-        SecondiPiatti secondo1 = new SecondiPiatti("Carpaccio di filetto con rucola e scaglie di parmigiano", 12.00);
-        SecondiPiatti secondo2 = new SecondiPiatti("Grigliata di scampi e gamberi", 16.0);
-        SecondiPiatti secondo3 = new SecondiPiatti("Impepata di cozze", 12.0);
-        SecondiPiatti secondo4 = new SecondiPiatti("Filetto di manzo alla tartara", 13.0);
-        SecondiPiatti secondo5 = new SecondiPiatti("Nodino di vitello alla griglia", 13.0);
-
-        SecondiPiatti.printSecondi();
-
-        System.out.println("\n");
-
-        Bevande bevanda1 = new Bevande("Acqua naturale", 1d);
-        Bevande bevanda2 = new Bevande("Acqua frizzante", 1d);
-        Bevande bevanda3 = new Bevande("Sprite", 3d);
-        Bevande bevanda4 = new Bevande("Coca cola", 3d);
-        Bevande bevanda5 = new Bevande("Vino", 0d);
-        Bevande.printBevande();
-
-        System.out.println("\n");
-
-
-        Dolci dolci1 = new Dolci("Tiramisù",12.5);
-        Dolci dolci2 = new Dolci("Torta della nonna",9.0);
-        Dolci dolci3 = new Dolci("Gelato",2.0);
-        Dolci dolci4 = new Dolci("Panna Cotta",5.0);
-        Dolci dolci5 = new Dolci("Macedonia di frutta",7.0);
-        Dolci.printDolci();
+        Ristorante.printMenu();
 
     }
 }
