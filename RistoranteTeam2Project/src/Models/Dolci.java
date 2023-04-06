9f1a33dc697f252b85672f1ec37f4b826e82ea3d
rickcicc <127338852+rickcicc@users.noreply.github.com>
2023-04-06 18:17:22 +0200

    aggiunta classe dolci

--- RistoranteTeam2Project/src/Models/Dolci.java
+++ RistoranteTeam2Project/src/Models/Dolci.java
@@ -1,2 +1,41 @@
-package Models;public class Dolci {
+package Models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dolci {
+    private String nomeDolci;
+    private double prezzoDolce;
+    public static Map<String, Double> dolci = new HashMap<>();
+
+    public Dolci(String nomeDolci, double prezzoDolci) {
+        this.nomeDolci = nomeDolci;
+        this.prezzoDolce = prezzoDolci;
+        dolci.put(nomeDolci, prezzoDolci);
+    }
+
+    public String getNomeDolci() {
+        return nomeDolci;
+    }
+
+    public void setNomeDolci(String nomeSecondo) {
+        this.nomeDolci = nomeSecondo;
+    }
+
+    public double getPrezzoDolce() {
+        return prezzoDolce;
+    }
+
+    public void setPrezzoDolce(double prezzoDolce) {
+        this.prezzoDolce = prezzoDolce;
+    }
+
+    public static void printDolci() {
+        System.out.println("Dolci :");
+
+        for (Map.Entry<String, Double> secondi : dolci.entrySet()) {
+            System.out.println(secondi.getKey() + " = " + secondi.getValue() + " Euro");
+        }
+    }
 }
+
