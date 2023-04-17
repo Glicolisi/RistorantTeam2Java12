4c58a58d4bccd841a5b65bffaee3be6d121732b3
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-04-17 17:56:27 +0200

    Estesa classe primi piatti

--- src/PrimiPiatti.java
+++ src/PrimiPiatti.java
@@ -1,39 +1,12 @@
 import java.util.*;
 
-public class PrimiPiatti {
+public class PrimiPiatti extends Portate {
 
-
-    private String nome;
-    private Double prezzo;
-
-    public PrimiPiatti(String nome, Double prezzo) {
-        this.nome = nome;
-        this.prezzo = prezzo;
+    public PrimiPiatti(String nome, Double prezzo, String ingredienti) {
+        super(nome, prezzo, ingredienti);
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getPrezzo() {
-        return prezzo;
-    }
-
-    public void setPrezzo(Double prezzo) {
-        this.prezzo = prezzo;
-    }
-
-    //TODO printInfo
-    public  void printMenuPrimipiatti(){
-        System.out.println(UtilityColor.GREEN+nome+": "+prezzo+" euro");
-
-    }
-
-
 }
+
+
 
 
