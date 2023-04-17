575ff95fb8a92f7a0437e21df2996c77b0c72877
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-04-17 17:45:24 +0200

    Aggiunta superclasse e colori enum

---
+++ src/Portate.java
@@ -0,0 +1,43 @@
+public class Portate {
+    private String nome;
+    private Double prezzo;
+    private String ingredienti;
+
+    public Portate(String nome, Double prezzo, String ingredienti) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.ingredienti = ingredienti;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getIngredienti() {
+        return ingredienti;
+    }
+
+    public void setIngredienti(String ingredienti) {
+        this.ingredienti = ingredienti;
+    }
+
+    public  void printInfo(String color){
+        System.out.println(getNome()+": "+getPrezzo()+" euro");
+
+        System.out.println(color+"("+getIngredienti()+")");
+
+    }
+
+}
---
+++ src/Utility.java
@@ -0,0 +1,56 @@
+public enum Utility {
+    //Background
+     ANSI_BLACK_BACKGROUND  ("\u001B[40m"),
+     ANSI_RED_BACKGROUND  ("\u001B[41m"),
+     ANSI_GREEN_BACKGROUND  ("\u001B[42m"),
+     ANSI_YELLOW_BACKGROUND  ("\u001B[43m"),
+     ANSI_BLUE_BACKGROUND  ("\u001B[44m"),
+     ANSI_PURPLE_BACKGROUND  ("\u001B[45m"),
+     ANSI_CYAN_BACKGROUND  ("\u001B[46m"),
+     ANSI_WHITE_BACKGROUND  ("\u001B[47m"),
+
+    // Regular Colors
+     BLACK  ("\033[0;30m"),   // BLACK
+     RED  ("\033[0;31m"),     // RED
+     GREEN  ("\033[0;32m"),   // GREEN
+     YELLOW  ("\033[0;33m"),  // YELLOW
+     BLUE  ("\033[0;34m"),    // BLUE
+     PURPLE  ("\033[0;35m"),  // PURPLE
+     CYAN  ("\033[0;36m"),    // CYAN
+     WHITE  ("\033[0;37m"),   // WHITE
+
+
+    // Bold
+    BLACK_BOLD  ("\033[1;30m"),  // BLACK
+    RED_BOLD  ("\033[1;31m"),    // RED
+    GREEN_BOLD  ("\033[1;32m"),  // GREEN
+    YELLOW_BOLD  ("\033[1;33m"), // YELLOW
+    BLUE_BOLD  ("\033[1;34m"),   // BLUE
+    PURPLE_BOLD  ("\033[1;35m"), // PURPLE
+    CYAN_BOLD  ("\033[1;36m"),   // CYAN
+    WHITE_BOLD  ("\033[1;37m"),  // WHITE
+
+    // Underline
+    BLACK_UNDERLINED  ("\033[4;30m"),  // BLACK
+    RED_UNDERLINED  ("\033[4;31m"),    // RED
+    GREEN_UNDERLINED  ("\033[4;32m"),  // GREEN
+    YELLOW_UNDERLINED  ("\033[4;33m"), // YELLOW
+    BLUE_UNDERLINED  ("\033[4;34m"),   // BLUE
+    PURPLE_UNDERLINED  ("\033[4;35m"), // PURPLE
+    CYAN_UNDERLINED  ("\033[4;36m"),   // CYAN
+    WHITE_UNDERLINED  ("\033[4;37m");  // WHITE
+
+    private String format;
+
+    Utility(String format) {
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+}
