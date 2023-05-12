b34920ee7cc5c4454fcf9023c4836e2876714675
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-05-12 09:01:43 +0200

    Aggiunta classe tavolo

---
+++ src/ristorante/Tavolo.java
@@ -0,0 +1,74 @@
+package ristorante;
+
+import enumartion.PosizioneEnum;
+import enumartion.TypesEnum;
+import portate.Portata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tavolo {
+    private Integer numero;
+    private PosizioneEnum posizione;
+    private TypesEnum tipo;
+    private boolean prenotazione;
+    private List<Cliente> clienteList;
+
+    public Tavolo(Integer numero, PosizioneEnum posizione, TypesEnum tipo, boolean prenotazione) {
+        this.numero = numero;
+        this.posizione = posizione;
+        this.tipo = tipo;
+        this.prenotazione = prenotazione;
+        this.clienteList=new ArrayList<>();
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public PosizioneEnum getPosizione() {
+        return posizione;
+    }
+
+    public TypesEnum getTipo() {
+        return tipo;
+    }
+
+    public boolean isPrenotazione() {
+        return prenotazione;
+    }
+
+    public void setPrenotazione(boolean prenotazione) {
+        this.prenotazione = prenotazione;
+    }
+
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void addCliente(Cliente cliente) {
+        clienteList.add(cliente);
+    }
+
+
+    public void removeCliente(Cliente cliente) {
+        clienteList.remove(cliente);
+    }
+
+
+    public void removeAllCliente() {
+
+        for (Cliente cliente : clienteList){
+
+            clienteList.remove(cliente);
+
+        }
+
+    }
+
+
+}
